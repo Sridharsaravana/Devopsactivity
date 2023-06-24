5e291f83b95cd3894545faf677e8d329fd7142c0
ubuntu user <ubuntu22@ubuntuuser.myguest.virtualbox.org>
2023-06-24 22:04:43 +0530

    Loops syntax script

---
+++ Loopssyntax.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+#Shell loops
+
+# if then else loop Syntax:
+
+if [condition]
+then
+	echo "Statement"
+else
+	echo "statement"
+
+
+# if then elif loop syntax:
+
+if[condition 1]
+then
+	echo "statement"
+
+elif[condtiton 2]
+then 
+	echo "statement"
+else
+	echo "statement"
+fi
+
+# For loop syntax:
+
+for varname in list
+do
+	echo "staement"
+done
+
+# while loop syntax:
+
+while [condition]
+do 
+	commands
+done
+	
